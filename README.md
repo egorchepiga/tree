e8a8157be323f3e0893417f5198086dc9eeff1bf
George Chepiga <egorchepiga@gmail.com>
2018-10-13 17:23:42 +0300

    GO tree utility

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# tree
+Утилита tree.
+
+Выводит дерево каталогов и файлов (если указана опция -f).
+```
+go run main.go testdata -f
+└───testdata
+   	├───project  (4096b)
+   	│	├───file.txt  (19b)
+   	│	└───gopher.png  (70372b)
+   	├───static  (4096b)
+   	│	├───a_lorem  (4096b)
+   	│	│	├───dolor.txt  (empty)
+   	│	│	├───gopher.png  (70372b)
+   	│	│	└───ipsum  (4096b)
+   	│	│	   	└───gopher.png  (70372b)
+   	│	├───css  (4096b)
+   	│	│	└───body.css  (28b)
+   	│	├───empty.txt  (empty)
+   	│	├───html  (4096b)
+   	│	│	└───index.html  (57b)
+   	│	├───js  (4096b)
+   	│	│	└───site.js  (10b)
+   	│	├───z_lorem  (4096b)
+   	│	│	├───dolor.txt  (empty)
+   	│	│	├───gopher.png  (70372b)
+   	│	│	└───ipsum  (4096b)
+   	│	│	   	└───gopher.png  (70372b)
+   	├───zline  (4096b)
+   	│	├───empty.txt  (empty)
+   	│	└───lorem  (4096b)
+   	│	   	├───dolor.txt  (empty)
+   	│	   	├───gopher.png  (70372b)
+   	│	   	└───ipsum  (4096b)
+   	│	   	   	└───gopher.png  (70372b)
+   	└───zzfile.txt  (empty)
+```
+
+```
+go run main.go testdata
+└───testdata
+   	├───project
+   	├───static
+   	│	├───a_lorem
+   	│	│	└───ipsum
+   	│	├───css
+   	│	├───html
+   	│	├───js
+   	│	└───z_lorem
+   	│	   	└───ipsum
+   	└───zline
+   	   	└───lorem
+   	   	   	└───ipsum
+```
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+type fsItem struct {
+	name string
+	path []string
+	last bool
+	size uint
+}
+
+func Tree(out *bytes.Buffer, path string, flag bool) error {
+	items, createError := createTree(path, flag)
+	if createError != nil {
+		fmt.Printf("error while creating dir tree: %v", createError)
+		return createError
+	}
+	treeString, printError := printTree(items, flag)
+	if printError != nil {
+		fmt.Printf("error while creating dir tree: %v", printError)
+		return printError
+	}
+	out.WriteString(treeString)
+	return nil
+}
+
+func createTree(path string, flag bool) ([]fsItem, error) {
+	items := make([]fsItem, 0, 20)
+	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		item := strings.Split(path, "/")
+		if flag || info.IsDir() {
+			items = append(items, fsItem{item[len(item)-1], item, false, uint(info.Size())})
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return nil, err
+	}
+	items[0].last = true
+
+	last := map[int]bool{1: true}
+	for i := len(items) - 1; i > 0; i-- {
+		pathLen := len(items[i].path)
+		if !last[pathLen] {
+			items[i].last = true
+			last[pathLen] = true
+		}
+		if len(items[i-1].path) > pathLen {
+			newLast := map[int]bool{pathLen: true}
+			for j := 0; j < len(items[i-1].path)-1; j++ {
+				newLast[j] = last[j]
+			}
+			last = newLast
+		}
+	}
+	return items, nil
+}
+
+func printTree(items []fsItem, flag bool) (string, error) {
+	result := fmt.Sprintf("└───%s \n", items[0].name)
+	indent := ""
+	for i := 1; i < len(items); i++ {
+		prevPathLen := len(items[i-1].path)
+		if prevPathLen < len(items[i].path) {
+			if items[i-1].last {
+				indent += "   \t"
+			} else {
+				indent += "│\t"
+			}
+		} else if prevPathLen > len(items[i].path) {
+			nestedLevel := prevPathLen - len(items[i].path)
+			indent = indent[:(len(indent) - (4 * nestedLevel))]
+		}
+
+		pref := ""
+		if items[i].last {
+			pref += "└───"
+		} else {
+			pref += "├───"
+		}
+		size := "empty"
+		if items[i].size > 0 {
+			size = strconv.Itoa(int(items[i].size)) + "b"
+		}
+
+		if flag {
+			result += fmt.Sprintf("%s%s%v  (%s) \n", indent, pref, items[i].name, size)
+		} else {
+			result += fmt.Sprintf("%s%s%v \n", indent, pref, items[i].name)
+		}
+	}
+	return result, nil
+}
+
+func main() {
+	out := os.Stdout
+	if !(len(os.Args) == 2 || len(os.Args) == 3) {
+		panic("usage go run main.go . [-f]")
+	}
+	path := os.Args[1]
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	buf := new(bytes.Buffer)
+	err := Tree(buf, path, printFiles)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintln(out, buf.String())
+}
